33e557a086551749f86cde6393b6ea509acea142
Anthony <antney64@yahoo.com>
2015-04-28 23:52:51 -0500

    Changes
    oh yeas i got it

--- app/src/main/java/group/cooking/IngredientsMenu.java
+++ app/src/main/java/group/cooking/IngredientsMenu.java
@@ -1,9 +1,18 @@
 package group.cooking;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+import java.util.Arrays;
 
 
 public class IngredientsMenu extends ActionBarActivity {
@@ -37,8 +46,63 @@ public class IngredientsMenu extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void storeIngredient(String ingred)
-    {
-
+    private boolean isEmpty(EditText etText) {
+        return etText.getText().toString().trim().length() == 0;
     }
+
+    private EditText input;
+    private int i =0;
+    private TextView displayInput;
+    private int count = 0;
+
+    public void buttonOnClick(View v)
+    {
+        //Button button = (Button) v;
+        input = (EditText) findViewById(R.id.editText);
+        if(isEmpty(input)){}
+        else
+            count++;
+        if(count > 0)
+            display();
+    }
+
+    String[] rd = new String[100];
+    String[] ingredients;
+    public void display(){
+        ingredients = new String[count];
+        rd[i++] = input.getText().toString();
+        for(int j = 0; j < ingredients.length; j++)
+        {
+            ingredients[j] = rd[j];
+        }
+        displayInput = (TextView) findViewById(R.id.savedIngred);
+        displayInput.setText(Arrays.toString(ingredients));
+        input.setText(null);
+    }
+
+    public void  clearButton(View v)
+    {
+        //Button button = (Button) v;
+        new AlertDialog.Builder(this)
+                .setTitle("Delete entry")
+                .setMessage("Are you sure you want to delete all of your saved ingredients?")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        deleteArray(rd);
+                    }
+                })
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // do nothing
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
+    public void deleteArray(String[] v){
+        displayInput.setText("");
+        String[] rd = new String[100];
+        count = 0;
+    }
+
 }
